474b9d47d27a40d2244f66cbfe12b09c4adfbc54
Zhen <106744948+Haoru-ZHEN@users.noreply.github.com>
2023-08-04 13:30:11 +0800

    backend

---
+++ Backend/utils/sendEmail.js
@@ -0,0 +1,33 @@
+//npm i nodemailer
+import nodemailer from 'nodemailer';
+//const nodemailer = require("nodemailer")
+
+export default async (email, subject, text) => {
+     try {
+          const transporter = nodemailer.createTransport({
+               host: 'smtp.gmail.com',
+               service: 'gmail',
+               post: 587,
+               secure: true,
+               auth: {
+                    user: 'zhenhaochok@gmail.com',
+                    pass: 'mmirvssocdisrzef',
+               },
+          });
+          //console.log(process.env.REACT_APP_USER);
+
+          //console.log(process.env.REACT_APP_EMAIL_PORT);
+
+          await transporter.sendMail({
+               from: process.env.REACT_APP_USER,
+               to: email,
+               subject: subject,
+               text: text,
+          });
+
+          console.log('Email sent successfully');
+     } catch (e) {
+          console.log('Email sent failed');
+          console.log(e);
+     }
+};
